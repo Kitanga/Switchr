64054884acf8533a1e72f9b40c897d6ea7e3af02
Kitanga <kitanganday@gmail.com>
2016-10-27 23:00:22 +0300

    Committing few changes before creating Doc branch

--- test/hide/main.js
+++ test/hide/main.js
@@ -1,4 +1,4 @@
-var groupKeys = ['step1', 'step2', 'step3', 'step4'],
+var groupKeys = ['1', '2', '3', '4'],
     groupIDs = ['step1', 'step2', 'step3', 'step4'],
     s1 = document.getElementById('s1'),
     s2 = document.getElementById('s2'),
@@ -7,17 +7,25 @@ var groupKeys = ['step1', 'step2', 'step3', 'step4'],
 
 /* New Format */
 var swr = Switchr;
+
 swr.init();
+
 var group = swr.group();
-group.add(['1', '2', '3', '4'], ['step1', 'step2', 'step3', 'step4']);
+
+group.add(groupKeys, groupIDs); //
+
 group.showAll();
 
 s1.onclick = function() {
-    group.hide('1');
+    group.hide(function() {
+        alert("First element is hidden!!!!");
+    });
 };
 
 s2.onclick = function() {
-    group.hide('2');
+    group.hide('2', function() {
+        alert("Second element is hidden!!!!");
+    });
 };
 
 s3.onclick = function() {
